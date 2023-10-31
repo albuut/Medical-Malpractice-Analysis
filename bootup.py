356883fcd53da0392b395e2e5c055f0e8869ec0e
albuut <heiygu@gmail.com>
2023-10-31 14:26:17 -0700

    updated bootup.py

--- bootup.py
+++ bootup.py
@@ -1,11 +1,9 @@
 import os
 
-lock_file_path = "/content/Medical-Malpractice-Analysis/lock_file.txt"
+lock_file_path = "/content/Medical-Malpractice-Analysis/.lock_file.txt"
 if os.path.exists(lock_file_path):
     print('script has already been ran')
 else:
     os.chdir('/content/Medical-Malpractice-Analysis/')
-    os.system('pip install pandas')
-    os.system('pip install scikit-learn')
     open(lock_file_path,'w').close()
     print('Google Colab initalized correctly')
