e023adde8cf0b7e271cb7c8d400a3abbe318fd28
albuut <heiygu@gmail.com>
2023-10-31 13:26:07 -0700

    Added process.py

---
+++ process_data.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import sys
+
+def dummy_variable_col(df, col):
+    df_temp = pd.get_dummies(df[col])
+    df_temp = df_temp.astype(int)
+    df = pd.concat([df,df_temp],axis=1,).reindex(df.index)
+    df.drop(col,axis=1,inplace=True)
+    return df
+
+## python3 function_name input output log_base
+errors = []
+
+if(len(sys.argv) == 4):
+    file_input = sys.argv[1]
+    file_output = sys.argv[2]
+    log_base = sys.argv[3]
+else:
+    errors.append("Invalid Number of Arguments")
+
+if(len(errors) == 0):
+    try:
+        df = pd.read_csv(file_input)
+    except Exception as e:
+        errors.append("Invalid file input / Could not find file")
+    
+    if(len(errors) == 0):
+        if log_base == '10':
+            df['log_Amount'] = np.log10(df['Amount'])
+        elif log_base == '2':
+            df['log_Amount'] = np.log2(df['Amount'])
+        elif log_base == 'ln':
+            df['log_Amount'] = np.log(df['Amount'])
+        else:
+            errors.append('Invalid 3rd Argument: Use 10, 2, or ln')
+        col_move = df.pop('log_Amount')
+        df.insert(loc=1,column='log_Amount',value=col_move)
+if(len(errors) == 0):
+    #Speciality
+    df = dummy_variable_col(df,'Specialty')
+    #Insurance
+    df = dummy_variable_col(df,'Insurance')
+    #Marital Status
+    df = dummy_variable_col(df,'Marital Status')
+    df = df.rename(columns={0:'Divorced', 1:'Single', 2:'Married', 3:'Widowed',4:'Unknown'})
+    #Gender Binary Encode
+    df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
+    df.to_csv(file_output, index=False)
+    print("Successfully processed data to: " + file_output)
+else:
+    for e in reversed(errors):
+        print(e)
