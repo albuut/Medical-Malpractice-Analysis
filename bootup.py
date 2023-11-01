b277535473e7c6b31c8a063cc2e7a583dd675375
Albert Nguyen <heiygu@gmail.com>
2023-10-31 18:31:43 -0700

    fixed issue

--- bootup.py
+++ bootup.py
@@ -4,7 +4,6 @@ lock_file_path = "/content/Medical-Malpractice-Analysis/.lock_file.txt"
 if os.path.exists(lock_file_path):
     print('script has already been ran')
 else:
-    os.system('!git clone https://github.com/albuut/Medical-Malpractice-Analysis.git')
     os.chdir('/content/Medical-Malpractice-Analysis/')
     open(lock_file_path,'w').close()
     print('Google Colab initalized correctly')
