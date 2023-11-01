1a6ae25f0067159c30914d30744521381abbec66
Albert Nguyen <heiygu@gmail.com>
2023-10-31 19:12:38 -0700

    added bootup.py

--- bootup.py
+++ bootup.py
@@ -2,8 +2,8 @@ import os
 
 lock_file_path = "/content/Medical-Malpractice-Analysis/.lock_file.txt"
 if os.path.exists(lock_file_path):
-    print('script has already been ran')
+    print('Script has already been ran.')
 else:
     os.chdir('/content/Medical-Malpractice-Analysis/')
     open(lock_file_path,'w').close()
-    print('Google Colab initalized correctly')
+    print('Google Colab initalized correctly.')
