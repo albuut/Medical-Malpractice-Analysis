05220f9d5c80e4bddafe5ae8fc4b13e8229b00e5
Albert Nguyen <heiygu@gmail.com>
2023-11-05 23:21:10 -0800

    formating

--- display_code.py
+++ display_code.py
@@ -5,6 +5,8 @@ from_line = int(sys.argv[2])
 to_line = int(sys.argv[3])
 
 with open(file_name, 'r') as file:
+    print(r'``` python')
     lines = file.readlines()[from_line-1:to_line]
     for line in lines:
         print(line)
+    print(r'```')
